8aae07c678ee83fc1dce2ef1edd392d5ea4007da
awakepanda <coloponpan@gmail.com>
2024-07-15 08:37:46 +0900

    style: gradient with css

---
+++ src/app/animation/page.tsx
@@ -0,0 +1,9 @@
+export default function Animation() {
+  return (
+    <div className="gradient-bg">
+      <div className="gradient">
+        <div className="g1"></div>
+      </div>
+    </div>
+  );
+}
