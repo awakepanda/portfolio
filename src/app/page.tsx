3f948fd53c00221655e89690879715a71d39b452
awakepanda <coloponpan@gmail.com>
2024-07-23 03:36:45 +0900

    feat: customise dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,15 @@ import { inter } from "./styles/fonts";
 export default function Home() {
   return (
     <main className="relative flex w-full h-screen text-base">
-      <ModeToggle />
-      <div className="w-1/2 h-screen py-[50px] px-[50px]">
+      <div className="w-1/2 h-screen py-[50px] px-[50px] bg-white">
         <h1 className={`${inter.className} text-3xl font-medium`}>
           SHINGO MAENO WORKS
         </h1>
       </div>
-      <div className="w-1/2 h-screen bg-primary">テスト</div>
+      <div className="w-1/2 h-screen">
+        <ModeToggle />
+        <p className="text-accent-foreground">テスト</p>
+      </div>
     </main>
   );
 }
