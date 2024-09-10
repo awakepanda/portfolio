99fd151a8591a23e672eaed448c82ccdc617122c
awakepanda <coloponpan@gmail.com>
2024-09-10 13:03:43 +0900

    delete unnecessary files

--- src/features/selfintroduction/components/AnimationLottieDot.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-
-export default function AnimationLottieDot() {
-  return <DotLottieReact src="./SmileAnimation2.lottie" autoplay loop />;
-}
--- src/features/selfintroduction/components/TextContent.tsx
+++ src/features/selfintroduction/components/TextContent.tsx
@@ -1,22 +1,7 @@
-"use client";
-
-import { useAnimationStore } from "@/store/animationStore";
-import { motion } from "framer-motion";
-
 export default function TextContent() {
-  // const { isRotating, isInitialAnimationComplete, triggerRotation } =
-  //   useAnimationStore();
-  //
-  // if (!isInitialAnimationComplete) {
-  //   return null;
-  // }
-
   return (
     <>
       <p>TextContent</p>
-      {/* <motion.button onClick={triggerRotation} disabled={isRotating}> */}
-      {/*   回転 */}
-      {/* </motion.button> */}
     </>
   );
 }
