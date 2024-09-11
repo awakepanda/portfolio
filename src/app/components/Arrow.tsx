927b4f58622e9b9b6496bffd25b893c79feb1574
awakepanda <coloponpan@gmail.com>
2024-09-11 12:50:58 +0900

    add svg data as a component

---
+++ src/app/components/Arrow.tsx
@@ -0,0 +1,18 @@
+export default function Arrow() {
+  return (
+    <div className="w-[calc((100vw*50)/1728)] absolute top-[calc((100vw*24)/1728) left-[calc((100vw*15)/1728)]">
+      <svg
+        width="41"
+        height="41"
+        viewBox="0 0 41 41"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M39.5071 40.51L39.5623 40.5652L39.6175 40.51H40.51L40.51 3.9754L39.51 3.9754L39.51 39.0987L1.22298 0.811692L0.515869 1.5188L38.5071 39.51L3.97537 39.51L3.97537 40.51H39.5071Z"
+          className="fill-light"
+        />
+      </svg>
+    </div>
+  );
+}
