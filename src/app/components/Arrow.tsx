75353dca2b38f9468ea0e866eae658b3876998f2
awakepanda <coloponpan@gmail.com>
2024-10-07 16:38:44 +0900

    feat: extended arrow component styles

--- src/app/components/Arrow.tsx
+++ src/app/components/Arrow.tsx
@@ -1,14 +1,37 @@
-export default function Arrow() {
+type ArrowStyleType = "default" | "left" | "right";
+
+const ARROW_STYLES: Record<ArrowStyleType, string> = {
+  default:
+    "absolute rotate-[225deg] w-sp-[16] t-sp-[14] l-sp-[8] md:w-tablet-[24] md:t-tablet-[20] md:l-tablet-[18] lg:w-pc-[44] lg:t-pc-[30] lg:l-pc-[30]",
+  left: "w-pc-[30]",
+  right: "w-pc-[30] rotate-180",
+};
+
+interface ArrowProps {
+  style?: ArrowStyleType;
+}
+
+export default function Arrow({ style = "default" }: ArrowProps) {
+  const arrowStyle = ARROW_STYLES[style];
+
   return (
-    <div className="absolute w-sp-[16] t-sp-[10] l-sp-[12] md:w-tablet-[20] md:t-tablet-[24] md:l-tablet-[18] lg:w-pc-[32] lg:t-pc-[36] lg:l-pc-[38]">
+    <div className={`${arrowStyle}`}>
       <svg
         className="w-full h-full"
-        viewBox="0 0 41 41"
+        viewBox="0 0 34 32"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M39.5071 40.51L39.5623 40.5652L39.6175 40.51H40.51L40.51 3.9754L39.51 3.9754L39.51 39.0987L1.22298 0.811692L0.515869 1.5188L38.5071 39.51L3.97537 39.51L3.97537 40.51H39.5071Z"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M1.00009 15.6022L34.0001 15.6022V16.6022L1.00009 16.6022L1.00009 15.6022Z"
+          className="fill-light"
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M1.75259 16.2272L16.6263 31.101L15.9192 31.8081L0.338379 16.2272L15.9192 0.646362L16.6263 1.35347L1.75259 16.2272Z"
           className="fill-light"
         />
       </svg>
