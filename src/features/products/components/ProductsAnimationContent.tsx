98a305b1ef38fa5b8dbfc20968dee310bb90d82b
awakepanda <coloponpan@gmail.com>
2024-10-07 18:47:04 +0900

    fix weather card size end add arrow object to select

--- src/features/products/components/ProductsAnimationContent.tsx
+++ src/features/products/components/ProductsAnimationContent.tsx
@@ -79,37 +79,39 @@ export default function ProductsAnimationContent() {
       onUpdate={handleAnimationUpdate}
       onAnimationComplete={handleAnimationComplete}
       style={{ transformStyle: "preserve-3d" }}
-      className="border-4 border-illust rounded-pc-[32] w-pc-[660] h-pc-[904] bg-background"
+      className="border-4 border-illust rounded-pc-[32] w-pc-[660] bg-background"
     >
       <motion.div
-        className="w-full h-full flex flex-col justify-end items-center pb-pc-[120]"
+        className="w-full h-full"
         style={{ backfaceVisibility: "hidden" }}
       >
-        {displayedWeather && WeatherIcon ? (
-          <>
-            <WeatherIcon />
-            <p className="text-illust font-inter font-medium leading-none mt-pc-[30] text-pc-[126]">
-              {displayedWeather.temp}°C
+        <div className="h-full flex flex-col justify-center items-center">
+          {displayedWeather && WeatherIcon ? (
+            <>
+              <WeatherIcon />
+              <p className="text-illust font-inter font-medium leading-none mt-pc-[24] text-pc-[110]">
+                {displayedWeather.temp}°C
+              </p>
+              <p className="flex items-center text-illust font-inter leading-none mt-pc-[12] text-pc-[30]">
+                <TbMapPinFilled />
+                <span>{displayedWeather.cityId}</span>/
+                <span>{displayedWeather.condition.toUpperCase()}</span>
+              </p>
+              <CitySelector />
+            </>
+          ) : (
+            <p className="text-illust font-notosansjp text-pc-[24]">
+              天気情報を取得中...
             </p>
-            <p className="flex items-center text-illust font-inter leading-none mt-pc-[40] text-pc-[36]">
-              <TbMapPinFilled />
-              <span>{displayedWeather.cityId}</span>/
-              <span>{displayedWeather.condition.toUpperCase()}</span>
-            </p>
-          </>
-        ) : (
-          <p className="text-illust font-notosansjp text-pc-[24]">
-            天気情報を取得中...
-          </p>
-        )}
-        <CitySelector />
+          )}
+        </div>
       </motion.div>
     </motion.div>
   );
 
   return (
     <div
-      className="w-full h-full flex justify-center items-center"
+      className="w-full h-full flex justify-center pt-pc-[130] pb-pc-[80]"
       style={{ perspective: "1000px" }}
     >
       <AnimatePresence mode="wait">
--- src/features/products/components/ProductsCitySelector.tsx
+++ src/features/products/components/ProductsCitySelector.tsx
@@ -5,16 +5,18 @@ export default function CitySelector() {
   const { selectedCity, setSelectedCity } = useWeatherStore();
 
   return (
-    <select
-      value={selectedCity}
-      onChange={(e) => setSelectedCity(e.target.value)}
-      className="py-pc-[18] pl-pc-[74] mt-pc-[130] w-pc-[385] text-pc-[30] font-notosansjp border rounded-full focus:outline-none focus:ring-0 bg-background border-4 border-illust text-illust"
-    >
-      {japanMejorCities.map((city) => (
-        <option key={city.id} value={city.id}>
-          {city.name}
-        </option>
-      ))}
-    </select>
+    <div className="relative mt-pc-[129] w-pc-[385] before:content-['▼'] before:absolute before:-translate-y-1/2 before:top-1/2 before:r-pc-[30] before:leading-none before:text-illust before:text-pc-[20]">
+      <select
+        value={selectedCity}
+        onChange={(e) => setSelectedCity(e.target.value)}
+        className="appearance-none cursor-pointer w-full font-notosansjp border rounded-full focus:outline-none focus:ring-0 bg-background border-4 border-illust text-illust py-pc-[18] pl-pc-[74] text-pc-[24]"
+      >
+        {japanMejorCities.map((city) => (
+          <option key={city.id} value={city.id}>
+            {city.name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
