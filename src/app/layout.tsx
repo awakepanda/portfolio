f6d48489a86d1ae4131cfabb056a119c6ae7bcec
awakepanda <coloponpan@gmail.com>
2024-10-13 03:53:08 +0900

    feat: add new favicon in svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,14 @@ const notosansjp = Noto_Sans_JP({
   subsets: ["latin"],
   weight: ["400", "500", "600"],
   variable: "--font-noto-sans-jp",
+  display: "swap",
 });
 
 const inter = Inter({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-inter",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -27,6 +29,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" suppressHydrationWarning>
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+      </head>
       <body
         className={`w-full overflow-hidden h-screen h-svh ${inter.variable} ${notosansjp.variable}`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import SplitLayout from "./layout/SplitLayout";
 import SelfIntroductionAnimationContent from "@/features/selfintroduction/components/SelfIntroductionAnimationContent";
 import SelfIntroductionTextContent from "@/features/selfintroduction/components/SelfIntroductionTextContent";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SELF-INTRODUCTION",
+};
 
 export default function SelfIntroduction() {
   return (
