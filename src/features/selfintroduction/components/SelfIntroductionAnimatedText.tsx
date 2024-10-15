a2047d15ee1af07f64e1d379cbeed3ba169b129f
awakepanda <coloponpan@gmail.com>
2024-10-16 05:43:28 +0900

    fix: text animation speed

--- src/features/selfintroduction/components/SelfIntroductionAnimatedText.tsx
+++ src/features/selfintroduction/components/SelfIntroductionAnimatedText.tsx
@@ -12,7 +12,7 @@ import { useAnimationStore } from "@/store/animationStore";
 import SelfIntroductionTogglePlayButton from "./SelfIntroductionTogglePlayButton";
 
 const ANIMATION_SPEED = 10;
-const SCROLL_DELAY = 2000;
+const SCROLL_DELAY = 3000;
 
 interface SelfIntroductionAnimatedTextProps {
   children: ReactNode;
